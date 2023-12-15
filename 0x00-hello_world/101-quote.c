e623592639ae94b130a91c13d3cf26deb641458c
gabermohamed18 <gabermohammed18@gmail.com>
2023-12-15 01:23:43 -0800

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <string.h>
 
 /**
  * main -Entry point
@@ -14,6 +15,6 @@ int main(void)
 	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	size_t len = strlen(quo);
 
-	write(1. &quo. 59);
+	write(1. &quo. len);
 	return (1);
 }
